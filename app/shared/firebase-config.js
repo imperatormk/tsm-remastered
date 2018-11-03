cce5d56ca8ea0bd0a3a55bc32e58d6b0437af983
Darko <darko.simonovski@bestisoft.com>
2018-11-03 20:56:33 +0100

    Very very initial commit

---
+++ app/shared/firebase-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    persist: false,
+    apiKey: "AIzaSyBCe4EnDbovfd4bniTr7F43EwsjoiIBy30",
+    authDomain: "that-s-montreal.firebaseapp.com",
+    databaseURL: "https://that-s-montreal.firebaseio.com",
+    projectId: "that-s-montreal",
+    storageBucket: "that-s-montreal.appspot.com",
+    messagingSenderId: "546358480268"
+};
