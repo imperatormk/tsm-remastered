f1466966a344ccf728e35fa3e3bc176d140ac293
Darko <darko.simonovski@bestisoft.com>
2018-11-04 23:38:38 +0100

    Added youtube video to media container

--- app/components/blocks/media/MediaContainer.vue
+++ app/components/blocks/media/MediaContainer.vue
@@ -1,8 +1,13 @@
 <template>
-  <Label :text="mediaItem.mediaId" backgroundColor="#50000000" horizontalAlignment="center"/>
+  <StackLayout>
+    <Label :text="mediaItem.mediaId" backgroundColor="#50000000" horizontalAlignment="center"/>
+    <YoutubePlayer id="player" @videoLoaded="videoLoaded" apiKey="AIzaSyDw-n7SxuoPBw3f1AqmEKu7xOZSFyMTJ0Y" :src="mediaItem.mediaUrl" width="100%" backgroundColor="gray"/>
+  </StackLayout>
 </template>
 
 <script>
+import MessageBus from '@/services/MessageBus'
+
 export default {
   props: {
     mediaItem: {
@@ -10,6 +15,18 @@ export default {
       required: true
     }
   },
+  methods: {
+    videoLoaded() {
+      this.playVideo()
+    },
+    playVideo() {
+      MessageBus.$emit('getPageRef', (ref) => {
+        const player = ref.getViewById('player')
+        player.playerStyle = 2
+        player.play()
+      })
+    }
+  }
 }
 </script>
 
--- app/components/containers/ViewContainer.vue
+++ app/components/containers/ViewContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <Page :verticalAlignment="loading ? 'center' : 'top'" actionBarHidden="true">
+  <Page :verticalAlignment="loading ? 'center' : 'top'" actionBarHidden="true" xmlns:ui="nativescript-youtubeplayer" ref="pageRef">
     <StackLayout v-if="loading">
       <LoadingIndicator/>
     </StackLayout>
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+import MessageBus from '@/services/MessageBus'
 import LoadingIndicator from '@/components/common/LoadingIndicator'
 
 export default {
@@ -16,6 +17,11 @@ export default {
       type: Boolean,
       default: true
     }
+  },
+  created() {
+    MessageBus.$on('getPageRef', (cb) => {
+      cb(this.$refs.pageRef.nativeView)
+    })
   },
   components: {
     LoadingIndicator
