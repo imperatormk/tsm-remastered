e9f0c2021ecad089ba1bce09558dbdae1bbca6aa
Darko <darko.simonovski@bestisoft.com>
2018-11-08 00:08:53 +0100

    Added color prop for loading indicator and changed the default color

--- app/components/common/LoadingIndicator.vue
+++ app/components/common/LoadingIndicator.vue
@@ -1,13 +1,17 @@
 <template>
   <StackLayout :height="full ? '100%' : 'auto'" verticalAlignment="center">
-    <ActivityIndicator color="red" busy="true" width="100" :height="full ? '100' : 'auto'"/>
+    <ActivityIndicator :color="color" busy="true" width="100" :height="full ? '100' : 'auto'"/>
   </StackLayout>
 </template>
 
 <script>
 export default {
   props: { // there can be one more prop for size
-    full: Boolean
+    full: Boolean,
+    color: {
+      type: String,
+      default: '#8c8c8c'
+    }
   }
 }
 </script>
