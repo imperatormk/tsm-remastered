0faef4d31e7d933d859ba9c4b21836645b55dace
Darko <darko.simonovski@bestisoft.com>
2018-11-08 02:41:14 +0100

    More FB like comment style

--- app/components/blocks/comments/CommentItem.vue
+++ app/components/blocks/comments/CommentItem.vue
@@ -1,11 +1,15 @@
 <template>
-  <StackLayout margin="10" borderBottomWidth="1">
-    <FlexboxLayout flexDirection="row" width="100%" paddingRight="10" paddingBottom="7">
-      <Image :src="commentItem.userProfImg" width="50" marginRight="5" style="border-radius:100%"/>
-      <FlexboxLayout flexDirection="column" justifyContent="space-around">
-        <Label :text="commentItem.userDispName" color="#8c8c8c"/>
-        <Label :text="commentItem.commContent" color="#8c8c8c"/>
-      </FlexboxLayout>
+  <StackLayout margin="10" marginTop="0" marginBottom="0">
+    <FlexboxLayout flexDirection="row" width="100%" paddingRight="10" paddingBottom="20">
+      <StackLayout minWidth="40" marginRight="5">
+        <Image :src="commentItem.userProfImg" width="40" minWidth="40" style="border-radius:100%"/>
+      </StackLayout>
+      <StackLayout width="100%">
+        <FlexboxLayout padding="5" flexDirection="column" justifyContent="space-around" backgroundColor="#f5f5f5" borderRadius="10">
+          <Label padding="2" :text="commentItem.userDispName" color="#8c8c8c"/>
+          <Label padding="2" fontSize="15" textWrap="true" :text="commentItem.commContent" color="#8c8c8c"/>
+        </FlexboxLayout>
+      </StackLayout>
     </FlexboxLayout>
   </StackLayout>
 </template>
