cdb822edc4beeaed44b3a6a0bc2998c32f96fff4
Darko <darko.simonovski@bestisoft.com>
2019-01-27 21:40:24 +0100

    Added more graceful buttons to the home screen

--- app/components/views/HomeScreen.vue
+++ app/components/views/HomeScreen.vue
@@ -1,9 +1,17 @@
 <template>
   <ViewContainer>
-    <StackLayout>
-      <Button text="Lines" @tap="goToScreen(1)"/>
-      <Button text="Social media" @tap="goToScreen(2)"/>
-    </StackLayout>
+    <FlexboxLayout flexDirection="column" alignItems="center" justifyContent="space-around" height="80%">
+      <Label text="That's Montreal!" fontSize="30" color="#8c8c8c"/>
+      <GridLayout columns="*" rows="*" width="300" height="300">
+        <FlexboxLayout col="0" row="0" flexWrap="wrap" alignItems="space-around" justifyContent="center">
+          <Button class="quarter top-left"></Button>
+          <Button @tap="goToScreen(1)" class="quarter top-right"></Button>
+          <Button class="quarter bottom-left"></Button>
+          <Button @tap="goToScreen(2)" class="quarter bottom-right"></Button>
+        </FlexboxLayout>
+        <StackLayout col="0" row="0" class="fill-circle"></StackLayout>
+      </GridLayout>
+    </FlexboxLayout>
   </ViewContainer>
 </template>
 
@@ -34,3 +42,36 @@ export default {
 }
 </script>
 
+<style lang="scss" scoped>
+  .quarter {
+    width: 50%;
+    height: 50%;
+  }
+
+  .fill-circle {
+    width: 65%;
+    height: 65%;
+    background-color:#000;
+    border-radius:100%;
+  }
+
+  .top-left {
+    border-top-left-radius: 200vw;
+    background: #ed8900;
+  }
+
+  .top-right {
+    border-top-right-radius: 200vw;
+    background: #007cc1;
+  }
+
+  .bottom-left {
+    border-bottom-left-radius: 200vw;
+    background: #0cad26;
+  }
+
+  .bottom-right {
+    border-bottom-right-radius: 200vw;
+    background: #ffd400;
+  }
+</style>
