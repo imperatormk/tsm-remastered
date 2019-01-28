2e0bd00058fc96b7ef8ec5e46bd202005ca370bf
Darko <darko.simonovski@bestisoft.com>
2019-01-28 01:35:15 +0100

    Improved contact us page

--- app/components/views/ContactUs.vue
+++ app/components/views/ContactUs.vue
@@ -1,33 +1,35 @@
 <template>
   <ViewContainer>
-    <FlexboxLayout flexDirection="column" width="100%" justifyContent="center" alignItems="center" height="80%">
-      <Label text="Contact us!" fontSize="30" color="#8c8c8c" marginBottom="30"/>
-      <FlexboxLayout flexDirection="column">
-        <TextField hint="Subject" 
-          v-model="subject" 
-          secure="false"
-          returnKeyType="done"
-          fontSize="15"
-          autocorrect="false"
-          padding="15"
-          width="90%"
-          style="margin:0px;border-width:3px;border-color:#e5e5e5"
-          class="input input-border"></TextField>
-        <StackLayout height="30"></StackLayout>
-        <TextView hint="Message"
-          v-model="message" 
-          secure="false"
-          textWrap="true"
-          returnKeyType="done"
-          fontSize="15"
-          autocorrect="false"
-          padding="15"
-          width="90%"
-          height="60%"
-          style="margin:0px;border-width:3px;border-color:#e5e5e5"
-          class="input input-border"></TextView>
-        <StackLayout height="10"></StackLayout>
-        <Button background="#0cad26" color="white" fontSize="22" text="Send"/>
+    <FlexboxLayout height="100%" alignItems="center">
+      <FlexboxLayout flexDirection="column" width="100%" justifyContent="center" alignItems="center" height="80%">
+        <Label text="Contact us!" fontSize="30" color="#8c8c8c" marginBottom="30"/>
+        <FlexboxLayout flexDirection="column">
+          <TextField hint="Subject" 
+            v-model="subject" 
+            secure="false"
+            returnKeyType="done"
+            fontSize="15"
+            autocorrect="false"
+            padding="15"
+            width="90%"
+            style="margin:0px;border-width:3px;border-color:#e5e5e5"
+            class="input input-border"></TextField>
+          <StackLayout height="20"></StackLayout>
+          <TextView hint="Message"
+            v-model="message" 
+            secure="false"
+            textWrap="true"
+            returnKeyType="done"
+            fontSize="15"
+            autocorrect="false"
+            padding="15"
+            width="85%"
+            height="60%"
+            style="margin:0px;border-width:3px;border-color:#e5e5e5"
+            class="input input-border"></TextView>
+          <StackLayout height="15"></StackLayout>
+          <Button class="btn-flat" background="#e5e5e5" color="#8c8c8c" fontSize="22" text="Send"/>
+        </FlexboxLayout>
       </FlexboxLayout>
     </FlexboxLayout>
   </ViewContainer>
@@ -51,3 +53,10 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .btn-flat {
+    border-color: transparent;
+    border-width: 1;
+  }
+</style>
