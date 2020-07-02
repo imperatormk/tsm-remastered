71d8184814dfc2503951747c1de6e2f45cca3be5
claudeldesir <48841539+claudeldesir@users.noreply.github.com>
2020-07-02 05:44:21 -0400

    Delete firebase-config.js

--- app/shared/firebase-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    persist: true,
-    apiKey: "AIzaSyCsKFd4B8n4p0qmH42BccPDWVzDD0QhtQU",
-    authDomain: "thatsmontrealcomments.firebaseapp.com",
-    databaseURL: "https://thatsmontrealcomments.firebaseio.com",
-    project_id: "thatsmontrealcomments"
-};
